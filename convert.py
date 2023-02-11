3c1d382b030c130df4205e2b10def9e10d1fc407
Sinan <sinan.teske@vorwerk.ch>
2023-02-11 23:01:03 +0100

    Changed structure according to parqet website.

--- convert.py
+++ convert.py
@@ -39,33 +39,81 @@ def convert(full_file_path = typer.Argument(..., help = "The path to the Swissqu
                         'Interne Titelumbuchung':'Interne Titelumbuchung',
                         'Vergütung':'TransferIn',
                         'Berichtigung Börsengeb.':'Berichtigung Börsengeb.'}
+    
+    mapping_cash_col = {'Datum':'datetime',
+               'Auftrag #':'Auftrag #',
+               'Transaktionen':'type',
+               'Symbol':'Symbol',
+               'Name':'Name',
+               'ISIN':'isin',
+               'Anzahl':'shares',
+               'Stückpreis':'price', 
+               'Kosten':'fee', 
+               'Aufgelaufene Zinsen':'Aufgelaufene Zinsen', 
+               'Nettobetrag':'Nettobetrag',
+               'Saldo':'Saldo',
+               'Währung':'originalcurrency'}
+    
+    
+    mapping_cash_transactions = {'Dividende':'TransferIn',
+                        'Kauf':'TransferOut', 
+                        'Rückzahlung':'TransferIn', 
+                        'Depotgebühren':'TransferOut',
+                        'Fx-Gutschrift Comp.':'TransferIn', 
+                        'Fx-Belastung Comp.':'TransferOut', 
+                        'Spin off':'Spin off',
+                        'Capital Gain':'TransferIn', 
+                        'Verkauf':'TransferIn', 
+                        'Forex-Gutschrift':'TransferIn', 
+                        'Forex-Belastung':'TransferOut',
+                        'Zins':'TransferIn',
+                        'Interne Titelumbuchung':'Interne Titelumbuchung',
+                        'Vergütung':'TransferIn',
+                        'Berichtigung Börsengeb.':'Berichtigung Börsengeb.'}
+    
     keep_columns = ['datetime',
                'type',
                'isin',
                'shares',
                'price',
                'fee',
+               'Nettobetrag',
                'originalcurrency']
     
     exp = imp.rename(columns=mapping_col)[keep_columns]
-    exp.replace(to_replace = mapping_transactions.keys(), value = mapping_transactions.values(), inplace = True)
+    exp.replace(to_replace = mapping_cash_transactions.keys(), value = mapping_cash_transactions.values(), inplace = True)
     exp['holding'] = ''
 
     exp.query("type in ['TransferIn','TransferOut'] and originalcurrency == 'CHF'").holding
-    exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'CHF'),'holding'] = 'hld_62fe9bf476362ac5e8b88ed5'
-    exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'USD'),'holding'] = 'hld_62fe9c0476362ac5e8b88ed8' # USD
-    exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'EUR'),'holding'] = 'hld_62fe9c1398683c34ff685ac2' # EUR
+    # exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'CHF'),'holding'] = 'hld_62fe9bf476362ac5e8b88ed5'
+    # exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'USD'),'holding'] = 'hld_62fe9c0476362ac5e8b88ed8' # USD
+    # exp.loc[((exp.type == 'TransferIn')|(exp.type == 'TransferOut')) & (exp.originalcurrency == 'EUR'),'holding'] = 'hld_62fe9c1398683c34ff685ac2' # EUR
+
+    exp.loc[ (exp.originalcurrency == 'CHF'),'holding'] = 'hld_63e7ac5e5b7760f5b9482775'
+    exp.loc[ (exp.originalcurrency == 'USD'),'holding'] = 'hld_63e7ac9d5ef1fc171a40af20' # USD
+    exp.loc[ (exp.originalcurrency == 'EUR'),'holding'] = 'hld_63e7ac9af527114e0a5f9b59' # EUR
 
     exp['datetime'] = pd.to_datetime(exp['datetime'], format='%d-%m-%Y %H:%M:%S')
     exp['datetime'] = exp["datetime"].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
     exp['currency'] = exp['originalcurrency']
     exp['tax'] = 0.0
     port = exp[exp['type'].isin(['Buy','Sell','Dividend','TransferIn','TransferOut'])]
-    cash = exp[exp['type'].isin(['TransferIn','TransferOut'])][['datetime','type','shares','price','fee','holding','tax']]
+    cash = exp
+    cash.replace(to_replace = mapping_transactions.keys(), value = mapping_transactions.values(), inplace = True)
+    cash = cash[['datetime','type','Nettobetrag','currency','fee','holding','tax']]
+    cash = cash[cash['type'].isin(['TransferIn','TransferOut'])]
+    cash.rename(columns={'Nettobetrag':'amount'},inplace=True)
+    cash['fee'] = 0
+    cash['tax'] = 0
+    cash['price'] = cash.amount.str.replace("-","")
+    cash['date'] = pd.to_datetime(cash['datetime']).dt.date
+    cash['time'] = ""
+    cash['shares'] = 1
+    cash = cash[['date','time','price','shares','currency','tax','fee','type','holding']]
+    
+    cash.to_csv('./cash.csv',sep=';', index = False)
 
-    cash.to_csv(dirname+'/cash.csv',sep=';', index = False)
-
-    exp.to_csv(dirname+'/portfolio.csv',sep=';', index = False)
+    exp.to_csv('./portfolio.csv',sep=';')
     
 
 
