6b0a35e876f4346d0063ca9dc0f068113fd97daa
Sinan <sinan.teske@vorwerk.ch>
2023-04-05 22:49:12 +0200

    changes on code cash working. portfolio import via parqet swissquote support.

--- convert.py
+++ convert.py
@@ -93,23 +93,27 @@ def convert(full_file_path = typer.Argument(..., help = "The path to the Swissqu
     exp.loc[ (exp.originalcurrency == 'USD'),'holding'] = 'hld_63e7ac9d5ef1fc171a40af20' # USD
     exp.loc[ (exp.originalcurrency == 'EUR'),'holding'] = 'hld_63e7ac9af527114e0a5f9b59' # EUR
 
+
     exp['datetime'] = pd.to_datetime(exp['datetime'], format='%d-%m-%Y %H:%M:%S')
     exp['datetime'] = exp["datetime"].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
     exp['currency'] = exp['originalcurrency']
     exp['tax'] = 0.0
     port = exp[exp['type'].isin(['Buy','Sell','Dividend','TransferIn','TransferOut'])]
     cash = exp
+    
     cash.replace(to_replace = mapping_transactions.keys(), value = mapping_transactions.values(), inplace = True)
     cash = cash[['datetime','type','Nettobetrag','currency','fee','holding','tax']]
     cash = cash[cash['type'].isin(['TransferIn','TransferOut'])]
     cash.rename(columns={'Nettobetrag':'amount'},inplace=True)
     cash['fee'] = 0
     cash['tax'] = 0
-    cash['price'] = cash.amount.str.replace("-","")
-    cash['date'] = pd.to_datetime(cash['datetime']).dt.date
+    
+    #cash['price'] = cash.amount.str.replace("-","") # if amount is string
+    cash['price'] = abs(cash.amount) # if amount is string
+    #cash['date'] = pd.to_datetime(cash['datetime']).dt.date
     cash['time'] = ""
     cash['shares'] = 1
-    cash = cash[['date','time','price','shares','currency','tax','fee','type','holding']]
+    cash = cash[['datetime','time','price','shares','currency','tax','fee','type','holding']]
     
     cash.to_csv('./cash.csv',sep=';', index = False)
 
